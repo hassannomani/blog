798146d378c5db5da337705ba7ed409c90232f48
hassan_nomani_alvi <alvi@monistit.com>
2018-12-25 21:46:52 +0600

    partisal errors

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -6,15 +6,7 @@
         Create a new post!
     </div>
     <div class="panel-body">
-        @if(count($errors) > 0)
-            <ul class="list-group">
-                @foreach($errors->all() as $error)
-                    <li class="list-group-item text-danger">
-                        {{ $error}}
-                    </li>
-                @endforeach
-            </ul>
-        @endif
+        @include('admin.includes.errors')
 
         <form action="{{ route('category.store') }}" method="post">
             {{ csrf_field() }}
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,16 +6,7 @@
         Create a new post!
     </div>
     <div class="panel-body">
-        @if(count($errors) > 0)
-            <ul class="list-group">
-                @foreach($errors->all() as $error)
-                    <li class="list-group-item text-danger">
-                        {{ $error}}
-                    </li>
-                @endforeach
-            </ul>
-        @endif
-
+        @include('admin.includes.errors')
         <form action="{{ route('post.create') }}" method="post" enctype="multipart/form-data">
             {{ csrf_field() }}
             <div class="form-group">
