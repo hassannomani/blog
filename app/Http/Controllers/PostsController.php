8bd794af138d0c4b0c3335416fe3ab3d8b620f00
hassan_nomani_alvi <alvi@monistit.com>
2018-12-29 00:11:16 +0600

    fillable

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 use App\Post;
-
+use Session;
 class PostsController extends Controller
 {
     /**
--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class Post extends Model
 {
+	use softDeletes;
+	protected $fillable = [
+        'title', 'content', 'featured','category_id'
+    ];
+    protected $dates = ['deleted_at'];
     public function category(){
     	return $this->belongsTo('App\Category');
     }
