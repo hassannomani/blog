8b1e381b55615ec2d971b39ff0419854d25263d7
redsaiyanAlpha <nomanialvi@gmail.com>
2019-02-20 16:01:08 +0600

    lecture 50

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
+        $user = App\User::create([
         	'name' => 'Hassan Nomani',
         	'email' => 'nomanialvi@gmail.com',
-        	'password' => bcrypt('123456')
+        	'password' => bcrypt('123456'),
+            'admin' => 1
+        ]);
+
+        App\Profile::create([
+            'user_id' => $user->id,
+            'avatar' => 'uploads/avatars/1.png',
+            'about' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil excepturi, nesciunt adipisci 
+                        minima dolorem quidem, maxime dignissimos, laborum cupiditate aperiam vero consequatur dolor 
+                        eius atque inventore placeat aspernatur ipsa voluptas!',
+            'facebook' => 'facebook.com',
+            'youtube' => 'youtube.com',
+
         ]);
     }
 }
